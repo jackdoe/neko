68981430bb8514ff2c04f7bbe7841f86fda6e47c
borislav nikolov <jack@sofialondonmoskva.com>
2017-06-12 17:22:22 +0200

    show something

--- nekoapp/app/components/Main.js
+++ nekoapp/app/components/Main.js
@@ -1,9 +1,67 @@
 import React, { Component } from 'react'
-import { Text, View, StatusBar } from 'react-native'
+import { View } from 'react-native'
+import { Button, Text } from 'react-native-elements'
+
 const data = require('../data')
+const shuffle = function (a) {
+  var j, x, i
+  for (i = a.length; i; i--) {
+    j = Math.floor(Math.random() * i)
+    x = a[i - 1]
+    a[i - 1] = a[j]
+    a[j] = x
+  }
+}
 
 export default class Main extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      picked: data.pick()
+    }
+  }
+
+  evaluate (choice) {
+    let right = {}
+    for (let r of this.state.picked.entries[0].SENSE[0].GLOSS) {
+      right[r] = true
+    }
+    let correct = false
+    for (let c of choice.entries[0].SENSE[0].GLOSS) {
+      if (right[c]) {
+        correct = true
+      }
+    }
+    this.setState({ correct: correct })
+    this.setState({ picked: data.pick() })
+  }
+
   render () {
-    return <Text>{JSON.stringify(data.pick())}</Text>
+    let picked = this.state.picked
+    let choices = [data.pick(), data.pick(), data.pick(), picked]
+    let buttons = choices.map((e, i) => {
+      let title = e.entries[0].SENSE[0].GLOSS.join(', ')
+
+      return (
+        <Button
+          key={i}
+          style={{ paddingTop: 5 }}
+          title={title}
+          onPress={() => this.evaluate(e)}
+        />
+      )
+    })
+
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1 }}>
+          <Text>{JSON.stringify(picked)}</Text>
+          <Text>correct: {this.state.correct ? 'true' : 'false'}</Text>
+        </View>
+        <View style={{ flex: 1 }}>
+          {buttons}
+        </View>
+      </View>
+    )
   }
 }
