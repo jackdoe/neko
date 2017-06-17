b5bc9cb2ab1568b18317b58e2a03c015905d5e9f
borislav nikolov <jack@sofialondonmoskva.com>
2017-06-17 16:06:31 +0200

    rebuild

---
+++ game/rebuild.sh
@@ -0,0 +1,3 @@
+sudo docker build . -t neko
+sudo docker kill $(sudo docker ps | grep 4567 | cut -f 1 -d ' ')
+sudo docker run -d -e "VIRTUAL_HOST=neko.science,www.neko.science" -e "LETSENCRYPT_HOST=neko.science,www.neko.science" -e "LETSENCRYPT_EMAIL=jack@prymr.nl" neko
