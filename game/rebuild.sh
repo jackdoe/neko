cc8ed4bd7f172a7234b04ccfc790a09d740db295
borislav nikolov <jack@sofialondonmoskva.com>
2017-06-17 22:54:20 +0200

    dont push webpack bundles

--- game/rebuild.sh
+++ game/rebuild.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 base=`dirname $0`
+sudo docker kill $(sudo docker ps | grep 4567 | cut -f 1 -d ' ') # no memory for everything :)
 cd $base/../sentences && sh copy.sh && cd -
+cd $base/../nekoapp && webpack --progress -p && cd -
 
 mvn clean package
 sudo docker build . -t neko
-sudo docker kill $(sudo docker ps | grep 4567 | cut -f 1 -d ' ')
+
 sudo docker run -d -e "VIRTUAL_HOST=neko.science,www.neko.science" -e "LETSENCRYPT_HOST=neko.science,www.neko.science" -e "LETSENCRYPT_EMAIL=jack@prymr.nl" neko
